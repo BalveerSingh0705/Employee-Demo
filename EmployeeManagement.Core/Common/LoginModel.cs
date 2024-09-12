398deaea0378fcb22804d0d89f7067e82c7df52d
Anurag Singh <balveersingh0705@outlook.com>
2024-09-12 14:40:45 +0530

    Login page done.

---
+++ EmployeeManagement.Core/Common/LoginModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Core.Common
+{
+    public class LoginModel
+    {
+        public string emailUsername { get; set; }  // Corresponds to emailUsername in the formData
+        public string password { get; set; }       // Corresponds to password in the formData
+        public bool rememberMe { get; set; }       // Corresponds to rememberMe in the formData
+    }
+
+
+        public class AuthLoginResponse
+        {
+            public bool IsSuccess { get; set; }
+            public string Token { get; set; }
+            public string ErrorMessage { get; set; }
+        }
+
+    public class AuthResponse
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public string Token { get; set; }
+        public int? UserId { get; set; }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; } // Store hashed passwords
+    }
+
+
+
+}
